85b72d4ee17ea4af40588a88211f521236125634
Sylvain Viollon <thefunny@gmail.com>
2010-06-24 14:13:37 +0000

    Review Zope configuration / boot.

--- src/infrae/wsgi/paster.py
+++ src/infrae/wsgi/paster.py
@@ -7,11 +7,35 @@ import pdb
 import sys
 
 from Zope2 import startup
-from Zope2.Startup.run import configure
 from infrae.wsgi.publisher import WSGIApplication
+from zope.event import notify
+from zope.processlifetime import ProcessStarting
 import Zope2
 
 logger = logging.getLogger('infrae.wsgi')
+
+
+def configure_zope(config_filename, debug_mode=False):
+    """Read zope.conf with zdaemon^Wzcrap.
+    """
+    from Zope2.Startup import options, handlers
+    import AccessControl
+    import App.config
+
+    del sys.argv[1:]
+    opts = options.ZopeOptions()
+    opts.configfile = config_filename
+    opts.realize(raise_getopt_errs=0)
+
+    handlers.handleConfig(opts.configroot, opts.confighandlers)
+    AccessControl.setImplementation(
+        opts.configroot.security_policy_implementation)
+    AccessControl.setDefaultBehaviors(
+        not opts.configroot.skip_ownership_checking,
+        not opts.configroot.skip_authentication_checking,
+        opts.configroot.verbose_security)
+    App.config.setConfiguration(opts.configroot)
+    set_zope_debug_mode(debug_mode)
 
 
 def set_zope_debug_mode(debug_mode):
@@ -24,14 +48,10 @@ def set_zope_debug_mode(debug_mode):
     Globals.DevelopmentMode = config.debug_mode
 
 
-def zope2_application_factory(global_conf, zope_conf, **options):
-    """Build a Zope2 WSGI application.
+def boot_zope(config_filename, debug_mode=False):
+    """Boot Zope.
     """
-    del sys.argv[1:]
-    debug_mode = options.get('debug_mode', 'off') == 'on'
-
-    configure(zope_conf)
-    set_zope_debug_mode(debug_mode)
+    starter = configure_zope(config_filename, debug_mode)
     logger.info("Zope configured")
 
     try:
@@ -46,7 +66,17 @@ def zope2_application_factory(global_conf, zope_conf, **options):
 
     # Some products / Zope code reset the debug mode. Re-set it again.
     set_zope_debug_mode(debug_mode)
+
+    # Notify start of application
+    notify(ProcessStarting())
     logger.info("Zope started")
+
+
+def zope2_application_factory(global_conf, zope_conf, **options):
+    """Build a Zope2 WSGI application.
+    """
+    debug_mode = options.get('debug_mode', 'off') == 'on'
+    boot_zope(zope_conf, debug_mode)
 
     return WSGIApplication(
         Zope2.bobo_application,
